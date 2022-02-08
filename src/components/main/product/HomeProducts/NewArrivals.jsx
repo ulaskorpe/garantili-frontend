9c65e12e9de9b8f63c9db2e470c003d74b63380d
Dnz Frt <denizfiratt03@gmail.com>
2022-02-08 14:40:40 +0300

    new arrivals baglandi

--- src/components/main/product/HomeProducts/NewArrivals.jsx
+++ src/components/main/product/HomeProducts/NewArrivals.jsx
@@ -80,83 +80,55 @@ class NewArrivals extends React.Component {
         this.state = {
             error: null,
             isLoaded: false,
-            products: {
-                title: 'Son Eklenenler',
-                headers: [
-                    { id: 1, title: "Telefonlar", tabUrl: "#weeklydeals" },
-                    { id: 2, title: "Tabletler", tabUrl: "#topsales" },
-                    { id: 3, title: "Aksesuarlar", tabUrl: "#newproducts" }
-                ],
-                tabs: [
-                    {
-                        id: 1, tabId: "weeklydeals", products: [
-                            { id: 8, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 9, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 10, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 11, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 12, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 13, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 14, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 15, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 16, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 17, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 18, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 19, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                            { id: 20, title: "Bluetooth on-ear PureBass Headphones", listPrice: "300.00", price: "500.00", url: "", imageUrl: "assets/images/products/7.jpg", discount: "150" },
-                        ]
-                    }
-                ]
-            },
+            products: null,
         };
     }
     componentDidMount() {
-        console.log('mounted')
-        //fetch("http://buyback.test/api/site/super-offer")
-        // fetch("https://buyback.garantiliteknoloji.com/api/site/super-offer")
-        // fetch("https://buyback.garantiliteknoloji.com/api/products/new-arrivals", {
-        //     headers: {
-        //         'x-api-key': '5c35640a3da4f1e3970bacbbf7b20e6c'
-        //     }
-        // })
-        //     .then((res) => res.json())
-        //     .then(
-        //         (result) => {
-
-        //             this.setState({ isLoaded: true, products: result });
-        //         },
-        //         (error) => {
-        //             this.setState({
-        //                 isLoaded: true,
-        //                 error,
-        //             });
-        //         }
-        //     );
+        fetch(`${process.env.REACT_APP_BASE}/api/products/new-arrivals`, {
+            headers: {
+                'x-api-key': '5c35640a3da4f1e3970bacbbf7b20e6c'
+            }
+        })
+            .then((res) => res.json())
+            .then(
+                (result) => {
+                    this.setState({ isLoaded:true, products: result });
+                },
+                (error) => {
+                    this.setState({
+                        isLoaded: true,
+                        error,
+                    });
+                }
+            );
     }
 
     render() {
         const { onAddToBasket } = this.props
+        const { isLoaded , products} = this.state
+        console.log(isLoaded,products)
         return (
             <section className="section-hot-new-arrivals section-products-carousel-tabs techmarket-tabs">
                 <header className="section-header">
-                    <h2 className="section-title arrival-title" >{this.state.products.title}</h2>
+                  {/* <h2 className="section-title arrival-title" >{this.state.products.title}</h2> */}
                     <ul role="tablist" className="nav justify-content-end">
-                        {this.state.products.headers.map((item, i) => {
+                        {isLoaded && products !== null && products.products.headers.map((item, i) => {
                             let classes = i === 0 ? "nav-link active" : "nav-link"
-                            return (<li className="nav-item" key={i}><a className={classes} href="#tab-59f89f0940f740" data-toggle="tab">{item.title}</a></li>)
+                            return (<li className="nav-item" key={i}><a className={classes} href={item.tabUrl} data-toggle="tab">{item.title}</a></li>)
                         })}
                     </ul>
                 </header>
                 <div className="tab-content">
-                    {this.state.products.tabs.map((item, i) => {
+                    {isLoaded && products !== null && products.products.tabs.map((item, i) => {
                         let classes = i === 0 ? "tab-pane active" : "tab-pane"
-                        return (<div className={classes} role="tabpanel" key={i}>
+                        return (<div className={classes} id={item.tabId} role="tabpanel" key={i}>
                             <div className="products-carousel" data-ride="tm-slick-carousel" data-wrap=".products"
                                 data-slick="{&quot;infinite&quot;:false,&quot;slidesToShow&quot;:7,&quot;slidesToScroll&quot;:7,&quot;dots&quot;:true,&quot;arrows&quot;:false,&quot;responsive&quot;:[{&quot;breakpoint&quot;:700,&quot;settings&quot;:{&quot;slidesToShow&quot;:2,&quot;slidesToScroll&quot;:2}},{&quot;breakpoint&quot;:780,&quot;settings&quot;:{&quot;slidesToShow&quot;:3,&quot;slidesToScroll&quot;:3}},{&quot;breakpoint&quot;:1200,&quot;settings&quot;:{&quot;slidesToShow&quot;:4,&quot;slidesToScroll&quot;:4}},{&quot;breakpoint&quot;:1400,&quot;settings&quot;:{&quot;slidesToShow&quot;:5,&quot;slidesToScroll&quot;:5}}]}">
                                 <div className="container-fluid">
                                     <div className="woocommerce">
                                         <div className="products">
                                             {item.products.map((_, j) => {
-                                                return <ProductItem product={_} onAddToBasket={onAddToBasket} key={j} />
+                                                return <ProductItem product={_} onAddToBasket={onAddToBasket} key={_.title} />
                                             })}
                                         </div>
                                     </div>
@@ -164,7 +136,6 @@ class NewArrivals extends React.Component {
                             </div>
                         </div>)
                     })}
-
                 </div>
             </section>
         );
