8814bdf04848975fd1f440173023dda0160bb65a
ulas.korpe <ulaskorpe@gmail.com>
2022-02-15 13:38:49 +0300

    approutes

--- src/App.jsx
+++ src/App.jsx
@@ -54,14 +54,30 @@ function App() {
     }
 
     return (
-        <BrowserRouter>
+     /*   <BrowserRouter>
             <Routes>
                 <Route path='/' element={<HomePage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} />} />
                 <Route path='/urunler' element={<ShopPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} />} />
                 <Route path='/urunler/:category-:categoryId' element={<ShopPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} />} />
                 <Route path='/urun-detay/:category-/:productId' element={<ProductPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} />} />
             </Routes>
-        </BrowserRouter>
+        </BrowserRouter>*/
+
+<BrowserRouter>
+<Routes>
+    <Route path='/' element={<HomePage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} homeContent={homeContent} />} />
+    <Route path='/urunler' element={<ShopPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} />} />
+    <Route path='/urunler/:category-:categoryId' element={<ShopPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} allProducts={allProduct} />} />
+    <Route path='/urun-detay/:category-:productId' element={<ProductPage basket={basket} addToBasket={addToBasket} removeFromBasket={removeFromBasket} allProducts={allProduct} />} />
+
+    <Route path='/sepet' element={<Cart basket={basket} removeFromBasket={removeFromBasket} />} />
+    <Route path='/odeme' element={<Payment basket={basket} removeFromBasket={removeFromBasket}  />} />
+    <Route path='/siparis-ozeti' element={<OrderSummary basket={basket} removeFromBasket={removeFromBasket}  />} />
+    <Route path='/login' element={<Login basket={basket} removeFromBasket={removeFromBasket}  />} />
+    <Route path='/login' element={<Faq basket={basket} removeFromBasket={removeFromBasket}  />} />
+</Routes>
+</BrowserRouter>
+
     );
 }
 
