2eacf8f2ce9e5cd98a75cb0256d04544d29108ba
ulas.korpe <ulaskorpe@gmail.com>
2022-03-09 11:25:56 +0300

    logout

--- src/components/pages/LogOut.jsx
+++ src/components/pages/LogOut.jsx
@@ -6,12 +6,19 @@ const LogOut = () => {
     const auth = useAuth();
     const navigate = useNavigate();
     const location = useLocation();
-
+/*
     useEffect(() => {
         auth?.logout();
         navigate('/login', { fromTo: location, replace: true });
     }, [auth, location, navigate]);
+*/
 
+useEffect(() => {
+    if (auth && auth.logout) {
+auth.logout();
+       navigate('/login', { fromTo: location, replace: true });
+}
+   }, [auth, location, navigate]);
     return <></>;
 };
 
