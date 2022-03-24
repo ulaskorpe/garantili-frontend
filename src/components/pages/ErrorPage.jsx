19a779c3a07ca8636d9495b262d431c741022260
Emin <emintayfur@icloud.com>
2022-03-24 16:16:01 +0300

    Hata sayfasında yapılan tasarımsal değişiklikler.

--- src/components/pages/ErrorPage.jsx
+++ src/components/pages/ErrorPage.jsx
@@ -1,4 +1,8 @@
 import React from 'react';
+import TopBar from "../layout/TopBar";
+import HeaderMain from "../layout/Header/Header";
+import BreadCrumb from "../layout/BreadCrumb";
+import Footer from "../layout/Footer/Footer";
 
 const errorMessages = {
     404: 'Aradığınız sayfa bulunamadı!',
@@ -6,14 +10,46 @@ const errorMessages = {
 }
 
 const ErrorPage = (props) => {
-    {/* Props */}
     const {
         code = 500,
     } = props;
 
+    const crumbs = [
+        { url: '#', title: code }
+    ]
+
     return (
-        <div>
-            {code} - {errorMessages[code] || errorMessages.unknown}
+        <div className="woocommerce-active single-product full-width normal">
+            <div id="page" className="hfeed site">
+                <TopBar/>
+                <HeaderMain/>
+            </div>
+            <div id="content" className="site-content" tabIndex="-1">
+                <div className="col-full">
+                    <div className="row">
+                        <BreadCrumb crumbs={crumbs}/>
+                        <div id="primary" className="content-area">
+                            <main id="main" className="site-main">
+                                <div className="type-page hentry">
+                                    <header className="entry-header border-no">
+                                        <div className="page-header-caption border-no text-center">
+                                            <h1 className="entry-title border-no">{code}</h1>
+                                        </div>
+                                    </header>
+                                    <div className="entry-content">
+                                        <div className="track-order w-100 text-center">
+                                            <p className="w-80 text-center ml-auto">
+                                                {errorMessages[code] || errorMessages.unknown}
+                                            </p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </main>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Footer/>
         </div>
     );
 };
