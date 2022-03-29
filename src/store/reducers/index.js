94eaaf55dc989150617bf76d6e1b3a4f1d9d05d8
Emin <emintayfur@icloud.com>
2022-03-29 17:03:06 +0300

    Temp için store kaydı yapıldı

--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -3,8 +3,10 @@ import { combineReducers } from 'redux';
 //
 import auth from "./auth";
 import basket from "./basket";
+import temp from "./temp";
 
 export const rootReducer = combineReducers({
     auth,
     basket,
+    temp,
 });
